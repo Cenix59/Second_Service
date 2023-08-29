0383c5f103a8c364d694d43d540a66f633602059
Cenix59 <118486024+Cenix59@users.noreply.github.com>
2023-08-29 09:11:27 -0300

    first commit

---
+++ Service/Service/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementService
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            IncrementService service = new IncrementService();
+
+            ServiceBase.Run(service);
+
+        }
+    }
+}
